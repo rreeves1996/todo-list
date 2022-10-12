cce0d8b36e16b31667ca221615b0748abb1b09fe
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-12 14:54:11 -0700

    Adding a list item works, deleting does not

--- src/components/tabs/Etc.jsx
+++ src/components/tabs/Etc.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { MDBListGroup, MDBListGroupItem, MDBCheckbox } from 'mdb-react-ui-kit';
+
 
 export default function Etc() {
+    const [items, setItems] = useState([])
+    const [itemCount, addItem] = useState(0);
+
+    const deleteItem = () => {
+        console.log(items)
+    }
+    const addNewItem = () => {
+        addItem(itemCount + 1);
+        setItems((items) => [...items, <Item key={itemCount} id={itemCount} deleteItem={deleteItem} />])
+    }
+
+    function Item({ id, deleteItem }) {
+        return (
+            <>
+                <MDBListGroupItem>
+                    <MDBCheckbox inline className="checkbox" key={id} id={id} />
+                    <input name="to-do-input" id="list-item-input"></input>
+                    <button onClick={() => deleteItem()}>Delete</button>
+                </MDBListGroupItem>
+                
+            </>
+        )
+    }
+
     return (
         <>
-            <h1>Etc</h1>
+            <h1>
+                <strong>Etc.</strong>
+            </h1>
+            <MDBListGroup style={{ minWidth: '22rem' }} light>
+                {items}
+            </MDBListGroup> 
+            <button onClick={() => addNewItem()}>Add Item</button>
         </>
     )
 }
